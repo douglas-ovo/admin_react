86ad57958890c2f8223bbee8fc2e462dbb707f00
liutao <lessthan4408@163.com>
2022-10-13 14:09:30 +0800

    echarts

---
+++ src/pages/Charts/index.tsx
@@ -0,0 +1,168 @@
+import React, { FC, useRef, useEffect } from 'react'
+import * as echarts from 'echarts'
+import style from './index.module.less'
+
+const option1 = {
+    tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+            // Use axis to trigger tooltip
+            type: 'shadow' // 'shadow' as default; can also be 'line' or 'shadow'
+        }
+    },
+    legend: {},
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis: {
+        type: 'value'
+    },
+    yAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    series: [
+        {
+            name: 'Direct',
+            type: 'bar',
+            stack: 'total',
+            label: {
+                show: true
+            },
+            emphasis: {
+                focus: 'series'
+            },
+            data: [320, 302, 301, 334, 390, 330, 320]
+        },
+        {
+            name: 'Mail Ad',
+            type: 'bar',
+            stack: 'total',
+            label: {
+                show: true
+            },
+            emphasis: {
+                focus: 'series'
+            },
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: 'Affiliate Ad',
+            type: 'bar',
+            stack: 'total',
+            label: {
+                show: true
+            },
+            emphasis: {
+                focus: 'series'
+            },
+            data: [220, 182, 191, 234, 290, 330, 310]
+        },
+        {
+            name: 'Video Ad',
+            type: 'bar',
+            stack: 'total',
+            label: {
+                show: true
+            },
+            emphasis: {
+                focus: 'series'
+            },
+            data: [150, 212, 201, 154, 190, 330, 410]
+        },
+        {
+            name: 'Search Engine',
+            type: 'bar',
+            stack: 'total',
+            label: {
+                show: true
+            },
+            emphasis: {
+                focus: 'series'
+            },
+            data: [820, 832, 901, 934, 1290, 1330, 1320]
+        }
+    ]
+}
+
+const option2 = {
+    tooltip: {
+        trigger: 'item'
+    },
+    legend: {
+        top: '5%',
+        left: 'center'
+    },
+    series: [
+        {
+            name: 'Access From',
+            type: 'pie',
+            radius: ['40%', '70%'],
+            avoidLabelOverlap: false,
+            itemStyle: {
+                borderRadius: 10,
+                borderColor: '#fff',
+                borderWidth: 2
+            },
+            label: {
+                show: false,
+                position: 'center'
+            },
+            emphasis: {
+                label: {
+                    show: true,
+                    fontSize: '40',
+                    fontWeight: 'bold'
+                }
+            },
+            labelLine: {
+                show: false
+            },
+            data: [
+                { value: 1048, name: 'Search Engine' },
+                { value: 735, name: 'Direct' },
+                { value: 580, name: 'Email' },
+                { value: 484, name: 'Union Ads' },
+                { value: 300, name: 'Video Ads' }
+            ]
+        }
+    ]
+}
+
+const Charts: FC<{}> = () => {
+    const renderRef = useRef(true)
+
+    const initChart: (dom: HTMLElement | null, option: any) => void = (dom, option) => {
+        let mychart
+
+        if (dom) {
+            mychart = echarts.init(dom)
+        }
+
+        (mychart as any).setOption(option)
+    }
+
+
+    useEffect(() => {
+        if (renderRef.current) { //防止钩子执行两次
+            renderRef.current = false
+            return
+        }
+
+        initChart(document.getElementById('mychart1'), option1)
+        initChart(document.getElementById('mychart2'), option2)
+    }, [])
+
+    return (
+        <div className={style.chart}>
+            <div id='mychart1' style={{ width: '500px', height: '500px' }}></div>
+            <div id='mychart2' style={{ width: '400px', height: '400px' }}></div>
+        </div>
+    )
+}
+
+export default Charts
+
