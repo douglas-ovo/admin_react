b67a3d32069c6992807e973c674b8b333234d094
liutao <lessthan4408@163.com>
2022-10-14 16:25:47 +0800

    局部less

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App() {
 
   return (
     <div className="App">
-      <Loading />
       <Suspense fallback={<Loading />}>
         {element}
       </Suspense>
--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -3,12 +3,12 @@ import style from './index.module.less'
 
 export default function Loading() {
     return (
-        <div className={style.load}>
-            <div className='load-wrapp'>
-                <div className="load-1">
-                    <div className="line"></div>
-                    <div className="line"></div>
-                    <div className="line"></div>
+        <div className={style['load']}>
+            <div className={style['load-wrapp']}>
+                <div className={style['load-1']}>
+                    <div className={style['line']}></div>
+                    <div className={style['line']}></div>
+                    <div className={style['line']}></div>
                 </div>
             </div>
         </div>
